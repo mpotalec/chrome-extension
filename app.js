bdd934b73322a956af9bc2fe7a6bd66e732e9747
mpotalec <matko321@hotmail.com>
2022-05-19 18:53:16 +0200

    not finished

---
+++ app.js
@@ -0,0 +1,14 @@
+let myLeads = ["ccc", "ttt"];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+
+inputBtn.addEventListener("click", function(){
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
+});
+
+for ( let i = 0; i < myLeads.length; i++ ){
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"  
+}
+
